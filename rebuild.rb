2f3cc3474d4979cc75fd081dff1aae75868858d1
Pierce Brooks <pbrooks@airsquirrels.com>
2019-06-06 10:23:13 -0400

    now checking for dependencies and specifying make parallelization

--- rebuild.rb
+++ rebuild.rb
@@ -38,6 +38,27 @@ def execute(arguments, options)
   return 0
 end
 
+def depend(pwd, target)
+  commands = ["libavcodec-dev", "libavdevice-dev", "libavfilter-dev", "libavformat-dev", "libswresample-dev", "libswscale-dev", "libssl-dev"]
+  for i in 0..(commands.length-1)
+    command = ["sudo", "apt-get", "build-dep", commands[i], "-y"]
+    if not (execute(command, []) == 0)
+      LOGGER.error("Depend failure (\"sudo apt-get build-dep\")?")
+      return -1
+    end
+  end
+  for i in 0..(commands.length-1)
+    if not (commands[i].include?("libav"))
+      command = ["sudo", "apt-get", "install", commands[i], "-y"]
+      if not (execute(command, []) == 0)
+        LOGGER.error("Depend failure (\"sudo apt-get install\")?")
+        return -1
+      end
+    end
+  end
+  return 0
+end
+
 def clone(pwd, target)
   command = ["git", "clone", "https://git.ffmpeg.org/ffmpeg.git", File.join(pwd, target)]
   if not (execute(command, []) == 0)
@@ -97,7 +118,6 @@ end
 
 def patch(pwd, target, version)
   patches = Dir.entries(File.join(pwd, version)).select{|entry| !File.directory?(entry)}
-  pataches = patches.sort
   for i in 0..(patches.length-1)
     path = patches[i]
     LOGGER.info("Patch: \"#{path}\"")
@@ -130,9 +150,9 @@ def configure(pwd, target)
   return 0
 end
 
-def make(pwd, target)
+def make(pwd, target, parallels)
   Dir.chdir(File.join(pwd, target))
-  command = ["make"]
+  command = ["make", "-j"+parallels]
   if not (execute(command, []) == 0)
     LOGGER.error("Make failure?")
     Dir.chdir(pwd)
@@ -164,7 +184,7 @@ def update(pwd, target)
   return 0
 end
 
-def run(version)
+def run(version, parallels)
   pwd = Dir.pwd
   target = "ffmpeg"
   if not (Dir.exists?(File.join(pwd, version)))
@@ -197,10 +217,13 @@ def run(version)
   if not (patch(pwd, target, version) == 0)
     return -1
   end
+  if not (depend(pwd, target) == 0)
+    return -1
+  end
   if not (configure(pwd, target) == 0)
     return -1
   end
-  if not (make(pwd, target) == 0)
+  if not (make(pwd, target, parallels) == 0)
     return -1
   end
   if not (install(pwd, target) == 0)
@@ -212,23 +235,42 @@ def run(version)
   return 0
 end
 
-LOGGER.info("Running...")
+def launch(arguments)
+  LOGGER.info("Running...")
 
-result = nil
+  result = nil
+  success = nil
+  
+  version = "n3.4"
+  parallels = "4"
 
-if not (ARGV.length == 0)
-  result = run(ARGV[0])
-else
-  result = run("n3.4")
-end
+  if (arguments.length > 0)
+    if (arguments.length > 1)
+      result = run(arguments[0], arguments[1])
+    else
+      result = run(arguments[0], parallels)
+    end
+  else
+    result = run(version, parallels)
+  end
 
-if not (result == nil)
-  if (result == 0)
-    LOGGER.info("Successful!")
+  if not (result == nil)
+    if (result == 0)
+      LOGGER.info("Successful!")
+      success = true
+    else
+      LOGGER.error("Error?")
+      success = false
+    end
   else
     LOGGER.error("Error?")
+    success = false
   end
-else
-  LOGGER.error("Error?")
+  
+  if not (success == nil)
+    return success
+  end
+  return false
 end
 
+launch(ARGV)
